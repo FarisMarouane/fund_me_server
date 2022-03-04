cc9bd117d2b6087acad83f7b6cefa451eea61dcc
Marouane Faris <faris.marouane@gmail.com>
2022-03-04 16:31:01 +0100

    Execute deploy and fund scripts from backend

--- index.js
+++ index.js
@@ -1,14 +1,88 @@
-const express = require('express')
-const app = express()
-const port = 5000
+import express, { json, urlencoded } from 'express';
+import { spawn } from 'child_process';
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true })) 
+const deploySmartContract = (fundRaiserAddress, campaignDuration) => {
+  let promise = new Promise((resolve, reject) => {
+    let fundProcess;
 
-app.post('/smart_contract', (req, res) => {
-  res.status(200);
-})
+    const deployProcess = spawn(
+      'brownie',
+      ['run', 'deploy.py', '--network', 'kovan'],
+      {
+        cwd: '/home/marouane/webDev/solidity/brownie_fund_me/smart_contract/scripts',
+        env: {
+          ...process.env,
+          fundRaiserAddress,
+          duration: campaignDuration,
+        },
+      },
+    );
+
+    deployProcess.stdout.on('data', function (data) {
+      console.log(`child process data: ${data}`);
+    });
+
+    deployProcess.on('error', (err) => {
+      reject(new Error('Deploy process error:', err));
+    });
+
+    deployProcess.on('exit', function (code, signal) {
+      console.log(
+        'Deploy process exited with ' + `code ${code} and signal ${signal}`,
+      );
+
+      if (code === 0) {
+        fundProcess = spawn(
+          'brownie',
+          ['run', 'fund.py', '--network', 'kovan'],
+          {
+            cwd: '/home/marouane/webDev/solidity/brownie_fund_me/smart_contract/scripts',
+          },
+        );
+
+        fundProcess.stdout.on('data', function (data) {
+          console.log(`child process data: ${data}`);
+        });
+
+        fundProcess.on('error', (err) => {
+          reject(new Error('Fund process err:', err));
+        });
+
+        fundProcess.on('exit', function (code, signal) {
+          console.log(
+            'Fund process exited with ' + `code ${code} and signal ${signal}`,
+          );
+          if (code === 0) {
+            resolve();
+          } else {
+            reject(new Error('Fund process exited with code:', code));
+          }
+        });
+      } else {
+        reject(new Error('Deploy process exited with code:', code));
+      }
+    });
+  });
+
+  return promise;
+};
+
+const app = express();
+const port = 5000;
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.post('/smart_contract', async (req, res) => {
+  const { campaignDuration, campaignOwnerAddress } = req.body;
+  try {
+    await deploySmartContract(campaignOwnerAddress, campaignDuration);
+    res.status(200).end();
+  } catch (error) {
+    res.status(500).end();
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
